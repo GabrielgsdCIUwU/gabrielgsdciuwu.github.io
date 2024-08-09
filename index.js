459b6815d35610defe3cc3fc8b0b49a27c457bae
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2024-08-09 17:59:42 +0200

    Server preparations

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+// Directorio actual
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "resources")));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
+
