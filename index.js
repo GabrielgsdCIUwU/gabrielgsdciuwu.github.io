fdd59ca409d27e6f466a8570b358065a79b576f5
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2025-11-06 21:33:33 +0100

    feat(index): Configurar express para usar ejs

--- index.js
+++ index.js
@@ -20,6 +20,9 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.use("/resources", express.static(path.join(__dirname, "resources")));
 app.use(webrouter);
 app.use("/api", apirouter);
