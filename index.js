24409cf668558688ae480435568f777dc8c56843
Gabrielgsd <104272301+GabrielgsdCIUwU@users.noreply.github.com>
2026-06-06 16:35:29 +0200

    feat(index): Add trust proxy

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const __dirname = path.dirname(__filename);
 dotenv.config({ path: path.join(__dirname, ".env") });
 
 const app = express();
+app.set("trust proxy", 1);
 app.use(express.json());
 app.disable("x-powered-by");
 
